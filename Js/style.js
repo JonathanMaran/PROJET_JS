1a0873093d5335b7f814f5fe86adc5de7f16cdcb
JonathanMaran <jonathan.maran@le-campus-numerique.fr>
2020-04-02 13:15:18 +0200

    Nouveau code Feed pour créatin de post (Fetch, Promesse...) #4

--- Js/style.js
+++ Js/style.js
@@ -1,40 +1,43 @@
 function fetchAllPosts() {
-    fetch("https://www.scorebat.com/video-api/v1/")
-        .then(response => response.json())
-        .then(res => {
-            const article = document.querySelector(".article");
-            res.forEach((post, i) => {
-                if(i < 5) {
-                    console.log(post);
-                    const title = document.createElement("h2");
-                    title.style.color = "white";
-                    const paragraph = document.createElement("div");
-                    title.innerHTML = post.title;
-                    paragraph.appendChild(title);
-                    const video = document.createElement('div');
-                    video.innerHTML = post.embed;
-                    paragraph.appendChild(video);
-                    article.appendChild(paragraph);
-
-                }
-            });
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => {
+            return response.json();
+        }) // analyser la réponse en JSON
+        .then(res => { //retour si resolve
+            fillTheSection(res);
+            console.log(res);
         })
-        .catch(error => console.log(error));
+        .catch(rej => {
+            console.log(rej); // retour si reject
+        });
 }
 
-// function createPost(dataPost) {
-//     const title = document.createElement("h2");
-//     const paragraph = document.createElement("p");
-//     title.innerHTML = dataPost.title;
-//     paragraph.appendChild(title);
-//     return paragraph;
-// }
-//
-// function handleData(data) { // fonction pour manipuler les données récupérer
-//     const article = document.querySelector(".article");
-//     data.forEach(post => {
-//         article.appendChild(createPost(post));
-//     });
-// }
+
+function createPost(dataPost) {
+    const section = document.createElement("section"); // création d'un élement section
+    section.className = "post box"; //
+    section.style.width = "48%";
+    section.style.backgroundColor = "red";
+    const title = document.createElement("h2");
+    title.style.color = "black";
+    title.innerHTML = dataPost.title.toUpperCase();
+    section.appendChild(title);
+    const body = document.createElement("div");
+    body.style.color = "white";
+    body.innerHTML = dataPost.body;
+    title.appendChild(body);
+
+    return section; // balise section > h2 > div vidéo (retourne aussi les enfants)
+}
+
+
+function fillTheSection(data) {// fonction pour manipuler les données récupérer
+    const main = document.querySelector('main'); // sélection de la balise main
+    data.forEach((post, i) => {
+        if (i < 5) { // limiter à 5 posts
+            main.appendChild(createPost(post, i)); // créer un enfant, avec la fonction createPost, au main
+        }
+    });
+}
 
 fetchAllPosts();
